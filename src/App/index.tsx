2212a0ac81b734d687813e4ddf511390b221444c
Matheus <matheus-rodrigues37@live.com>
2022-11-21 19:27:04 -0300

    fix bug

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,57 +1,14 @@
-import { useRef, useState } from "react";
-import {
-  MinusButton, PlayButton, PlusButton, StopButton
-} from '../components/Buttons';
+import { useRef } from 'react';
+import { MinusButton, PlayButton, PlusButton, StopButton } from '../components/Buttons';
 import Clock from "../components/Clock";
 import Sounds from "../components/Sounds";
+import useTimer from '../hooks/useTimer';
 import { formatTime } from "../Logic/formatTime";
 import { Buttons, Container, Timer } from './styles';
 
-
 const App = () => {
-  const [timer, setTimer] = useState(0)
-  const [isActive, setIsActive] = useState(false)
-  const [isPaused, setIsPaused] = useState(false)
-  const countRef = useRef<number>(0)
-
-  const handleStart = () => {
-    setIsActive(true)
-    setIsPaused(true)
-    countRef.current = setInterval(() => {
-      setTimer((timer) => timer + 1)
-    }, 1000)
-  }
-
-  const handleReset = () => {
-    clearInterval(countRef.current)
-    setIsActive(false)
-    setIsPaused(false)
-    setTimer(0)
-  }
-
-  const handlePause = () => {
-    clearInterval(countRef?.current)
-    setIsPaused(false)
-    setIsActive(false)
-  }
-
-  const handleResume = () => {
-    setIsPaused(true)
-    countRef.current = setInterval(() => {
-      setTimer((timer) => timer + 1)
-    }, 1000)
-  }
-
-  const handlePlus = () => {
-    setTimer((prevState) => prevState + 300)
-  }
-
-  const handleMinus = () => {
-    if (timer > 0) {
-      setTimer((prevState) => prevState - 300)
-    }
-  }
-
+  const countRef = useRef(0)
+  const { timer, isActive, isPaused, handleStart, handlePause, handleResume, handleReset, handlePlus, handleMinus } = useTimer()
   return (
     <Container>
       <Timer>
@@ -65,7 +22,7 @@ const App = () => {
           }
           <StopButton onClickStop={handleReset} />
           <PlusButton onClickPlus={handlePlus} />
-          <MinusButton onClickMinus={handleMinus}/>
+          <MinusButton onClickMinus={handleMinus} />
         </Buttons>
       </Timer>
       <Sounds />
---
+++ src/hooks/useTimer.ts
@@ -0,0 +1,60 @@
+import { useRef, useState } from "react";
+
+const useTimer = (initialState = 0) => {
+  const [timer, setTimer] = useState(initialState);
+  const [isActive, setIsActive] = useState(false);
+  const [isPaused, setIsPaused] = useState(false);
+  const countRef = useRef(0);
+
+  const handleStart = () => {
+    setIsActive(true);
+    setIsPaused(true);
+    countRef.current = setInterval(() => {
+      setTimer((timer) => timer + 1);
+    }, 1000);
+  };
+
+  const handlePause = () => {
+    clearInterval(countRef.current);
+    setIsPaused(false);
+    setIsActive(false);
+  };
+
+  const handleResume = () => {
+    setIsPaused(true);
+    countRef.current = setInterval(() => {
+      setTimer((timer) => timer + 1);
+    }, 1000);
+  };
+
+  const handleReset = () => {
+    clearInterval(countRef.current);
+    setIsActive(false);
+    setIsPaused(false);
+    setTimer(0);
+  };
+
+  const handlePlus = () => {
+    setTimer((prevState) => prevState + 300);
+  };
+
+  const handleMinus = () => {
+    if (timer > 0) {
+      setTimer((prevState) => prevState - 300);
+    }
+  };
+
+  return {
+    timer,
+    isActive,
+    isPaused,
+    handleStart,
+    handlePause,
+    handleResume,
+    handleReset,
+    handlePlus,
+    handleMinus,
+  };
+};
+
+export default useTimer;
